29c7673f8ac9603a87f57482e3044fedc74eeff7
Roberto Zen <roberto.zen@skytv.it>
2018-12-17 14:32:08 +0100

    feat(node): connection with Redis Cluster added

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,7 +1,26 @@
-const express = require('express')
-const app = express()
+const Express = require('express')
+const Redis = require('ioredis')
+const app = Express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+
+    const cluster = new Redis.Cluster([{
+        port: 7002,
+        host: 'localhost'
+    }, {
+        port: 7003,
+        host: 'localhost'
+    }])
+
+    cluster.set('foo', 'bar')
+    cluster.get('foo', function(err, res) {
+        console.log(err)
+        console.log(res)
+    })
+
+
+    res.send('Hello World!');
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
